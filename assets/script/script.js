c08442aa7fb7ffb4e5ed81b53dfc4b68de195046
Woutnak <Kantoramanandraibe01@gmail.com>
2023-12-13 12:32:08 +0300

    ajoute fonctionalite ajout de liste des taches

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,20 @@ const inputAdd = document.getElementById('input-add')
 const btnAdd = document.getElementById('btn-add')
 const displayTask = document.querySelector('.display-task')
 
-btnAdd.addEventListener('click', () =>{
-    console.log("HEloo");
+btnAdd.addEventListener("click", (event) =>{
+    event.preventDefault(); // Empêche le comportement par défaut du clic 
+    let task = inputAdd.value
+    let taskList = document.createElement("ul")
+    let tasks = document.createElement("li")
+    if (task != " ") {
+        tasks.innerHTML =`<input type="checkbox">
+                          ${task}
+                           <div>
+                           <button>Edit</button> 
+                           <button>Deleted</button>
+                           </div> ` 
+        taskList.appendChild(tasks)
+        displayTask.appendChild(taskList)
+    }
+    inputAdd.value = " "
 })
